afbacb7978ca7be7c2e6a4ce2b8142e207ad9f2c
R-Romanov <alximikpb@mail.ru>
2017-02-21 18:40:44 +0200

    Добавил поддержку парсинга приватных постов

--- FbPost.class.php
+++ FbPost.class.php
@@ -3,11 +3,15 @@
 /**
  * Class FbPost - служит для получения содержимого поста из Facebook по его ссылке.
  *
+ * Если пост общедоступный (от публичной страницы) получает его содержимое через Facebook API
  * Использует id и secret приложения разработчика класса.
  * При необходимости, их можно изменить обратившись к методам
  * setAppId($appId) и setAppSeret($appSecret)
  *
- * Пример исспользования:
+ * Если пост не общедоступный (посты от пользователей, посты от некоторых групп)
+ * Получает содержимое страницы, разбирает построчно и выбирает содержимое
+ *
+ * Пример использования:
  * $postUrl = 'https://www.facebook.com/groups/kharkovgroup/permalink/1452263168118586/';
  * $post = new FbPost($postUrl);
  * echo $post->getData();
@@ -16,37 +20,33 @@
  */
 class FbPost
 {
-    private $id;
+    private $postUrl;
     private static $appId = '348981855502122';
     private static $appSecret = 'fbe0c8aefe76aea75cc49e95b82ed6f9';
 
     public function __construct($_postUlr)
     {
-        $this->id = $this->getIdByLink($_postUlr);
-    }
-
-
-    /**
-     * @param $_postUlr
-     * @return array
-     * @throws Exception если из ссылки не удаётся выделить id поста
-     */
-    private function getIdByLink($_postUlr){
-        preg_match('/(?<=permalink\/)[0-9]{14,16}/', $_postUlr, $postId);
-        if (empty($postId)) {
-            throw new Exception('Не удаётся выделить id поста из заявленной ссылки!');
-        }
-        return end($postId);
+        $this->postUrl = $_postUlr;
     }
 
     /**
-     * Устанавливает id поста
+     * Возвращает ссылку на пост
      *
-     * @param mixed $id
+     * @return mixed
      */
-    public function setId($id)
+    public function getPostUrl()
     {
-        $this->id = $id;
+        return $this->postUrl;
+    }
+
+    /**
+     * Устанавливает ссылку на пост
+     *
+     * @param mixed $postUrl
+     */
+    public function setPostUrl($postUrl)
+    {
+        $this->postUrl = $postUrl;
     }
 
     /**
@@ -68,15 +68,59 @@ class FbPost
     {
         self::$appId = $appId;
     }
+    
 
     /**
-     * Возвращает id поста
-     *
-     * @return mixed
+     * @param $_postUlr
+     * @return array
+     * @throws Exception если из ссылки не удаётся выделить id поста
      */
-    public function getId()
+    private function getIdByLink($_postUlr){
+        preg_match('/(?<=permalink\/)[0-9]{14,16}/', $_postUlr, $postId);
+        if (empty($postId)) {
+            throw new Exception('Не удаётся выделить id поста из заявленной ссылки!');
+        }
+        return end($postId);
+    }
+
+
+    /**
+     * Получает html код страницы с постом
+     *
+     * @return string
+     * @throws Exception если не удаётся получить содержимое страницы
+     */
+    private function getPostPageSource()
     {
-        return $this->id;
+        $ch = curl_init($this->postUrl);
+        curl_setopt($ch, CURLOPT_POST, false);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $data = curl_exec($ch);
+        if(!$data){
+            throw new Exception('Не удаётся получить содержимое страницы!');
+        }
+        return curl_exec($ch);
+    }
+
+    /**
+     * Получает html код содержимого поста
+     *
+     * @param $_string
+     * @return string
+     */
+    private function getPostContentSource($_string)
+    {
+        $start = 'userContent"';
+        $end = '</div>';
+
+        $_string = ' ' . $_string;
+        $ini = strpos($_string, $start);
+        if ($ini == 0) return '';
+        $ini += strlen($start);
+        $len = strpos($_string, $end, $ini) - $ini;
+        return substr($_string, $ini, $len);
     }
 
     /**
@@ -87,18 +131,32 @@ class FbPost
      */
     public function getData()
     {
-        $response = file_get_contents(
-            'https://graph.facebook.com/v2.8/'
-            . urlencode($this->id)
-            . '?access_token=' . self::$appId . '|' . self::$appSecret);
 
-        $response = json_decode($response);
+        if (strpos($this->postUrl, 'permalink')){
 
-        if (isset($response->message)) {
-            return $response->message;
+            $postId = $this->getIdByLink($this->postUrl);
+
+            $response = file_get_contents(
+                'https://graph.facebook.com/v2.8/'
+                . urlencode($postId)
+                . '?access_token=' . self::$appId . '|' . self::$appSecret);
+
+            $response = json_decode($response);
+
+            if (isset($response->message)) {
+                return $response->message;
+            } else {
+                throw new Exception('Не удаётся получить содержимое поста!');
+            }
+
         } else {
-            throw new Exception('Не удаётся получить содержимое поста!');
+            $postSource = $this->getPostContentSource($this->getPostPageSource());
+            if(!$postSource){
+                throw new Exception('Не удаётся получить содержимое поста!');
+            }
+            return strip_tags('<div' . $postSource);
         }
+
     }
 
 }
--- index.php
+++ index.php
@@ -2,16 +2,18 @@
 include_once('FbPost.class.php');
 
 $postUrls = [
-        'https://www.facebook.com/groups/kharkovgroup/permalink/1452263168118586/',
-        'https://www.facebook.com/groups/natalia83/permalink/1713299582021444/',
-        'https://www.facebook.com/groups/1756616864597062/permalink/1849312168660864/',
-    ];
+    'https://www.facebook.com/dubilet/posts/10155074024513552',
+    'https://www.facebook.com/mariani.alexandr/posts/1070148669743524?pnref=story',
+    'https://www.facebook.com/groups/kharkovgroup/permalink/1452263168118586/',
+    'https://www.facebook.com/groups/natalia83/permalink/1713299582021444/',
+    'https://www.facebook.com/groups/1756616864597062/permalink/1849312168660864/',
+];
 
-foreach ($postUrls as $postUrl){
+foreach ($postUrls as $postUrl) {
     echo 'Получаем содержимое поста: ' . $postUrl . "<br>";
 
     $post = new FbPost($postUrl);
-    echo $post->getData(). "<hr>";
+    echo $post->getData() . "<hr>";
 }
 
 ?>
